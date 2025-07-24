740e08461a44a2fa7f0c14a0b746cd3416ae04f2
salihgundogan <salih.gundogan@istun.edu.tr>
2025-07-24 16:26:30 +0300

    v3 baslangic

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { useAuthStore } from './stores/auth.store';
 
 const authStore = useAuthStore();
 // Sayfa ilk yüklendiğinde veya yenilendiğinde, token varsa kullanıcı bilgilerini tekrar çek.
-// Bu, "sayfa yenilenince bilgi kaybolmasın" isteğini karşılar.
+// Bu, "sayfa yenilenince bilgi kaybolmasın" isteğini karşılar...
 onMounted(() => {
     if (authStore.isAuthenticated) {
         authStore.fetchUser();
